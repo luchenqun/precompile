e83ff1aded28b5f5a96ed46832ee1239523f66dc
Luke <luchenqun@qq.com>
2024-04-09 16:15:00 +0800

    add staking precompile contract

--- staking/validator.js
+++ staking/validator.js
@@ -6,32 +6,6 @@ BigInt.prototype.toJSON = function () {
   return this.toString();
 };
 
-const convertArrayToObject = (dataArray, abi) => {
-  const parsedAbi = abi;
-  const validatorObject = {};
-
-  parsedAbi.forEach((item, index) => {
-    if (item.type === 'function' && item.outputs) {
-      item.outputs.forEach((output, outputIndex) => {
-        if (output.type === 'tuple') {
-          const tupleValues = dataArray[index];
-          const tupleObject = {};
-          output.components.forEach((component, componentIndex) => {
-            tupleObject[component.name] = tupleValues[componentIndex];
-          });
-          validatorObject[item.name] = tupleObject;
-        } else {
-          validatorObject[item.outputs[outputIndex].name] = dataArray[index];
-        }
-      });
-    } else {
-      validatorObject[item.name] = dataArray[index];
-    }
-  });
-
-  return validatorObject;
-};
-
 export const main = async () => {
   const { rpc, contracts, stakingAddress } = await fs.readJSON('../cfg.json');
   const { abi } = await fs.readJSON(path.join(contracts, 'staking/IStaking.sol/IStaking.json'));
@@ -42,7 +16,7 @@ export const main = async () => {
 
   const staking = new ethers.Contract(stakingAddress, abi, provider);
   const validator = await staking.validator(validatorAddress);
-  console.log('validator', JSON.stringify(validator.toObject(), undefined, 2), validator.description.moniker);
+  console.log('validator', JSON.stringify(validator.toObject(), undefined, 2));
 };
 
 main();
