1be48ba22afe9a8bbd55e9ecc73f1a1bb5d0c722
Luke <luchenqun@qq.com>
2024-07-29 09:31:21 +0800

    feat: add proposal for test

---
+++ gov/15_test.js
@@ -0,0 +1,79 @@
+import { ethers } from 'ethers';
+import fs from 'fs-extra';
+import path from 'path';
+
+BigInt.prototype.toJSON = function () {
+  return this.toString();
+};
+
+export const main = async () => {
+  const { rpc, contracts, govAddress, denom } = await fs.readJSON('../cfg.json');
+  const { abi } = await fs.readJSON(path.join(contracts, 'gov/IGov.sol/IGov.json'));
+  const provider = new ethers.JsonRpcProvider(rpc);
+
+  // input params
+  const privateKey = 'e54bff83fc945cba77ca3e45d69adc5b57ad8db6073736c8422692abecfb5fe2';
+  const wallet = new ethers.Wallet(privateKey, provider);
+  const gov = new ethers.Contract(govAddress, abi, wallet);
+
+  {
+    const messages = JSON.stringify([
+      {
+        '@type': '/cosmos.gov.v1.MsgExecLegacyContent',
+        authority: 'ethos10d07y265gmmuvt4z0w9aw880jnsr700j78lu59',
+        content: {
+          '@type': '/cosmos.params.v1beta1.ParameterChangeProposal',
+          title: 'Bank Module Parameter Change: DefaultSendEnabled.',
+          description: 'change default_send_enabled from true to false',
+          changes: [
+            {
+              subspace: 'bank',
+              key: 'DefaultSendEnabled',
+              value: 'false',
+            },
+          ],
+        },
+      },
+    ]);
+    const metadata = 'Just Test Proposal';
+    const initialDeposit = [
+      {
+        denom,
+        amount: '10000000',
+      },
+    ];
+
+    const tx = await gov.submitProposal(messages, initialDeposit, metadata);
+    const receipt = await tx.wait();
+    console.log('legacy submit proposal success, receipt: ', receipt);
+  }
+
+  {
+    const status = 0;
+    const voter = '0x0000000000000000000000000000000000000000';
+    const depositor = '0x0000000000000000000000000000000000000000';
+    const pageRequest = {
+      key: '0x00',
+      offset: 0,
+      limit: 100,
+      countTotal: true,
+      reverse: false,
+    };
+
+    const [_, pageResponse] = await gov.proposals(status, voter, depositor, pageRequest);
+    const proposalId = pageResponse.toObject(true).total.toString();
+
+    const options = [
+      { option: 1, weight: '0.6' },
+      { option: 2, weight: '0.1' },
+      { option: 3, weight: '0.2' },
+      { option: 4, weight: '0.1' },
+    ]; // 0:Unspecified, 1:Yes, 2: Abstain, 3: No, 4:NoWithWeto
+    const metadata = 'hello, use evm tx voteWeighted gov';
+    const tx = await gov.voteWeighted(proposalId, options, metadata);
+    const receipt = await tx.wait();
+    console.log(`vote proposal success, blockNumber: ${receipt.blockNumber}, blockHash: ${receipt.blockHash}`);
+  }
+};
+
+main();
